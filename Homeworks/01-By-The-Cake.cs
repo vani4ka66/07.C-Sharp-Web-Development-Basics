9eabd46a0535a6850bea10ef90a93883bba4564b
Vania Vasileva <vani4ka66@abv.bg>
2017-12-12 12:56:19 +0200

    Commit

---
+++ Homeworks/01-By-The-Cake.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace 01-By-The-Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Content-type: text/html\r\n");
+            Console.WriteLine("<!DOCTYPE html>\r\n<html lang=\"en\">\r\n    <head>\r\n        <meta charset=\"UTF-8\">\r\n        <title>By The Cake</title>\r\n            </head>\r\n   <body>\r\n       <h1>The Cake</h1>\r\n       <h2>Enjoy our awesome cakes</h2>\r\n   </body>\r\n</html>");
+        }
+    }
+}
